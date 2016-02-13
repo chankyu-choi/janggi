8195631a406a6dd2beab3ac7ab40d5b47ce1bff9
ChankyuChoi <nagyojang@gmail.com>
2016-02-13 10:15:59 +0900

    add man-to-computer mode

--- janggi/main.cpp
+++ janggi/main.cpp
@@ -12,14 +12,16 @@ using namespace std;
 
 bool string2ints(string in, Pos& current, Pos& next);
 void autoMode(Janggi& janggi);
-void manaualMode(Janggi& janggi);
+void man2Computer(Janggi& janggi);
+void manualMode(Janggi& janggi);
 
 int main()
 {
   Janggi janggi;
   janggi.Print();
 
-  autoMode(janggi);
+  //autoMode(janggi);
+  man2Computer(janggi);
   //manualMode(janggi);
 
   return 1;
@@ -43,7 +45,41 @@ void autoMode(Janggi& janggi)
     } while(1);
 }
 
-void manaualMode(Janggi& janggi)
+void man2Computer(Janggi& janggi)
+{
+  string inputs;
+  Pos current, next;
+  Turn turn;
+
+  do {    
+    turn = TURN_CHO;
+    cout << "Act : ";
+    getline(cin, inputs);
+    
+    bool isCorrect = true;
+    do {
+      isCorrect = string2ints(inputs, current, next);
+      if (isCorrect) {
+        janggi.PerformAction(Action(current, next));
+        janggi.Print();
+      } else {
+        cout << "Type the correct command" << endl;
+      }
+
+    } while (!isCorrect);
+
+    turn = TURN_HAN;
+    clock_t start = clock();
+    Action action = janggi.CalculateNextAction(turn);
+    double duration = (clock() - start) / (double)CLOCKS_PER_SEC;
+    cout << "max depth : " << MINMAX_DEPTH << endl;
+    cout << "calc time : " << duration << endl;
+    janggi.PerformAction(action); //throws on error. use 'try-catch' to handle exception.
+    janggi.Print();    
+  } while (1);
+}
+
+void manualMode(Janggi& janggi)
 {
     Pos current, next;
     string inputs;
